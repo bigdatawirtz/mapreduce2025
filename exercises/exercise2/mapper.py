9f2c6cb51c9cda215cabf3d7253c36cbae95e739
ivaanleeal <ivanleallopez1.com>
2026-01-08 18:54:53 +0100

    Terminado 1.2

--- exercises/exercise2/mapper.py
+++ exercises/exercise2/mapper.py
@@ -4,6 +4,6 @@ for line in sys.stdin:
     data = line.strip().split("\t")
     if len(data) == 5:
         datetime, store, item, cost, payment = data
-        print(store+'\t'+cost)
+        print(item+'\t'+cost)
     else:
         continue
