e5b87fad8eb8e9907957ce4aa869bd6711ec0516
Sheyk <efergo93@gmail.com>
2026-01-11 22:41:08 +0100

    ejercicio 1 finalizado

--- exercises/exercise1/reducer.py
+++ exercises/exercise1/reducer.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python
 import sys
+
 salesTotal = 0
 oldKey = None
+
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
     if len(data_mapped) != 2:
-        # Something has gone wrong. Skip this line.
         continue
     thisKey, thisSale = data_mapped
-    # Escribe un par key:value ante un cambio na key
-    # Reinicia o total
     if oldKey and oldKey != thisKey:
-        print(oldKey+"\t"+str(salesTotal))
-        oldKey = thisKey
+        print "%s\t%.2f" % (oldKey, salesTotal)
         salesTotal = 0
-        oldKey = thisKey
+    oldKey = thisKey
     salesTotal += float(thisSale)
-# Escribe o ultimo par, unha vez rematado o bucle
-if oldKey != None:
-    print(oldKey+"\t"+str(salesTotal))
+
+if oldKey is not None:
+    print "%s\t%.2f" % (oldKey, salesTotal)
